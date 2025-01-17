ceea02845c8f53539e35cbc3cc75a28571bf708a
jackzengh <jackzengh@gmail.com>
2025-01-18 09:24:41 +1100

    Initial Commit

---
+++ TodoListTests/TodoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListTests.swift
+//  TodoListTests
+//
+//  Created by Jack Zheng on 18/1/2025.
+//
+
+import Testing
+@testable import TodoList
+
+struct TodoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
